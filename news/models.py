a5028748f8bf9fc1426f67a1a80f6599922d53a0
Vlada <pudova.vlada@yandex.ru>
2023-04-15 13:56:51 +0300

    Init Skillfactory

--- news/models.py
+++ news/models.py
@@ -23,6 +23,9 @@ class Author(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=64, unique=True)
 
+    def __str__(self):
+        return self.name.title()
+
 
 class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -49,6 +52,9 @@ class Post(models.Model):
 
     def preview(self):
         return '{} ... {}'.format(self.text[0:124], self.rating)
+
+    def __str__(self):
+        return f'{self.title.title()}: {str(self.dateCreation)[:10]} {self.text[:20]}'
 
 
 class PostCategory(models.Model):
